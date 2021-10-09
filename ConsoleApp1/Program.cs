12182f5862e08fbb6a9a2686925b84d87793fc87
m1212 <m1212@LAPTOP-8TLI6PA5>
2021-10-09 17:53:07 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace restless
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double R, h;
+            Console.Write("R=");
+            R = Convert.ToDouble(Console.ReadLine());
+            Console.Write("h=");
+            h = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Объём цилиндра: {3.14 * R * R * h}");
+            Console.WriteLine($"Объём конуса: {(3.14 * R * R * h) * 1 / 3}");
+        }
+    }
+}
+
